5f32da1cec442d6683bd41e4d83d45909261e2d1
Joseph Timbol <joseph2000k@gmail.com>
2020-12-22 16:01:21 +0800

    AppNavbar done 3rd video of traversy media

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,17 @@
+import AppNavbar from './components/AppNavbar';
 import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css'
+import { Component } from 'react';
 
-function App() {
-  return (
-    <div className="App">
-      <h1>Hello</h1>
-    </div>
-  );
+class App extends Component{
+  render(){
+    return(
+      <div className="App">
+        <AppNavbar />
+      </div>
+    );
+  }
 }
+  
 
 export default App;
